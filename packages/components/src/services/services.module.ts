29bcf0f5aa9d2283113117cb8b6f608431695e9b
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2023-10-29 12:30:50 -0300

    fix: add config module to services module

--- packages/components/src/services/services.module.ts
+++ packages/components/src/services/services.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { sequelizeProvider } from './sequelize';
 import { serviceProviders } from './service-providers.enum';
 
+const dirName = dirname(fileURLToPath(import.meta.url));
+const envFilePath = resolve(dirName, '..', '..', '..', '..', '.env');
+
+@Global()
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [resolve(envFilePath)],
+    }),
+  ],
   providers: [sequelizeProvider],
   exports: [serviceProviders.SEQUELIZE],
 })
