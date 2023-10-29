4aa8da4494fdce2cc3c77ff93449c1a8effc0e2e
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2023-10-29 18:17:46 -0300

    feat: add migration for market reports

---
+++ packages/components/sequelize-cli/migrations/20231029210225-create-market-report.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('market_reports', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+      city: {
+        type: Sequelize.ENUM(
+          'BRIDGEWATCH',
+          'LYMHURST',
+          'FORTSTERLING',
+          'THETFORD',
+          'MARTLOCK',
+          'CAERLEON',
+          'BRECILIEN'
+        ),
+        allowNull: false,
+      },
+      buy_entries: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      sell_entries: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('market_reports');
+  }
+};
