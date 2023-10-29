cd56a74610e633450e510964bb05fbebd90be159
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2023-10-29 18:16:52 -0300

    feat: add enum for cities

---
+++ packages/components/src/enums/index.ts
@@ -0,0 +1,9 @@
+export const enum Cities {
+  Bridgewatch = 'Bridgewatch',
+  Caerleon = 'Caerleon',
+  FortSterling = 'Fort Sterling',
+  Lymhurst = 'Lymhurst',
+  Martlock = 'Martlock',
+  Thetford = 'Thetford',
+  Brecilien = 'Brecilien',
+}
