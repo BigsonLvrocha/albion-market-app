f06fc46305f2850dbda33f548618e1ea57948f58
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2023-10-29 16:17:16 -0300

    fix: use context only app

--- packages/components/src/test/beforeEach.ts
+++ packages/components/src/test/beforeEach.ts
@@ -2,8 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ServicesModule, serviceProviders } from '../services';
 
 export async function beforeEach(): Promise<void> {
-  const app = await NestFactory.create(ServicesModule);
-  await app.init();
+  const app = await NestFactory.createApplicationContext(ServicesModule);
   const sequelize = app.get(serviceProviders.SEQUELIZE);
   await sequelize.query(`
     delete from items;
