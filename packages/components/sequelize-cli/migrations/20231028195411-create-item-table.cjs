beb8edb3eb21d4146bd9abffeac54c00acf55838
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2023-10-29 15:01:25 -0300

    fix: addmissing columns to table

--- packages/components/sequelize-cli/migrations/20231028195411-create-item-table.cjs
+++ packages/components/sequelize-cli/migrations/20231028195411-create-item-table.cjs
@@ -13,6 +13,14 @@ module.exports = {
         type: Sequelize.TEXT,
         allowNull: false,
         unique: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
       }
     });
   },
