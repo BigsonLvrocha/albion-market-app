207462ee29e67d6dbec7bf68f234ba82674784ff
github.com/BigsonLvrocha <luizvictor.rocha@gupy.com.br>
2023-11-06 09:07:46 -0300

    fix: fix delete

--- packages/components/src/test/beforeEach.ts
+++ packages/components/src/test/beforeEach.ts
@@ -4,10 +4,8 @@ import { ServicesModule, serviceProviders } from '../services';
 export async function beforeEach(): Promise<void> {
   const app = await NestFactory.createApplicationContext(ServicesModule);
   const sequelize = app.get(serviceProviders.SEQUELIZE);
-  await sequelize.query(`
-    delete from market_reports;
-    delete from items;
-  `);
+  await sequelize.query(`delete from market_reports;`);
+  await sequelize.query('delete from items');
   await app.close();
 }
 
